1f6dec63431de11d5a864dea9cfb0940dd4e3afb
aniketqw <aniket21stcentury@gmail.com>
2024-08-25 22:12:22 +0530

    removed  error of redeclaring webSocket

--- chat-starter-code/server.js
+++ chat-starter-code/server.js
@@ -37,9 +37,9 @@ const server = http.createServer((req, res) => {
 
 // TODO
 // Exercise 3: Create the WebSocket Server using the HTTP server
-const WebSocket=require('ws');
+
 //httpServer is a premade HTTP server
-const wsServer=new WebSocket.Server({server:httpServer});
+const wsServer=new WebSocket.Server({server});
 
 // TODO
 // Exercise 5: Respond to connection events 
